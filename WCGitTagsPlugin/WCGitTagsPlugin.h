81a6cfa9b90816e965d5a8cd7afac8464f66e66e
wczekalski <wczekalski@me.com>
2014-03-25 13:29:10 +0100

    Initial Commit

---
+++ WCGitTagsPlugin/WCGitTagsPlugin.h
@@ -0,0 +1,13 @@
+//
+//  WCGitTagsPlugin.h
+//  WCGitTagsPlugin
+//
+//  Created by Wojciech Czekalski on 25.03.2014.
+//  Copyright (c) 2014 Wojciech Czekalski. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface WCGitTagsPlugin : NSObject
+
+@end
---
+++ WCGitTagsPlugin/WCGitTagsPlugin.m
@@ -0,0 +1,64 @@
+//
+//  WCGitTagsPlugin.m
+//  WCGitTagsPlugin
+//
+//  Created by Wojciech Czekalski on 25.03.2014.
+//    Copyright (c) 2014 Wojciech Czekalski. All rights reserved.
+//
+
+#import "WCGitTagsPlugin.h"
+
+static WCGitTagsPlugin *sharedPlugin;
+
+@interface WCGitTagsPlugin()
+
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation WCGitTagsPlugin
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource acccess
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
